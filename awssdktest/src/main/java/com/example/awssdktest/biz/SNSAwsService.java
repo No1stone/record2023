306b58aab30de31ed5fca8b7bcf4de12984a02ea
jangwonseok <jangws1003.io@gmail.com>
2022-08-25 08:35:42 +0900

    swagger project add

--- awssdktest/src/main/java/com/example/awssdktest/biz/SNSAwsService.java
+++ awssdktest/src/main/java/com/example/awssdktest/biz/SNSAwsService.java
@@ -1,38 +1,16 @@
 package com.example.awssdktest.biz;
 
 import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.MessageAttributeValue;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
-import com.google.gson.Gson;
-import com.sparwk.externalconnectnode.mailing.biz.v1.dto.AccountResponse;
-import com.sparwk.externalconnectnode.mailing.biz.v1.dto.AdminEmailSendRequest;
-import com.sparwk.externalconnectnode.mailing.config.common.VerifyNumber;
-import com.sparwk.externalconnectnode.mailing.config.filter.responsepack.Response;
-import com.sparwk.externalconnectnode.mailing.config.filter.responsepack.ResultCodeConst;
-import com.sparwk.externalconnectnode.mailing.config.filter.token.UserInfoDTO;
-import com.sparwk.externalconnectnode.mailing.jpa.dto.MediaVerifyBaseDTO;
-import com.sparwk.externalconnectnode.mailing.jpa.entity.MediaVerify;
-import com.sparwk.externalconnectnode.mailing.jpa.repository.MediaVerifyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.support.MessageSourceAccessor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
