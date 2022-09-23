406e6a8c18fce14e2b08032d6ca3ad9efd438942
jangwonseok <jangws1003@naver.com>
2022-09-23 11:02:56 +0900

    config version add

---
+++ feignclientConfig/src/main/java/com/example/feignclientconfig/FeignclientConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.feignclientconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignclientConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignclientConfigApplication.class, args);
+    }
+
+}
---
+++ feignclientConfig/src/test/java/com/example/feignclientconfig/FeignclientConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feignclientconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignclientConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
