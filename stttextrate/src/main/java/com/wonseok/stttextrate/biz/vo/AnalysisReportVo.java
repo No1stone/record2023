479e46fc0306686e97b47348dd566860a6dfe219
jangwonseokicom <jangws1003@icomsys.co.kr>
2023-05-02 17:23:32 +0900

    자카드알고리즘 문장비교 add

--- stttextrate/src/main/java/com/wonseok/stttextrate/biz/vo/AnalysisReportVo.java
+++ stttextrate/src/main/java/com/wonseok/stttextrate/biz/vo/AnalysisReportVo.java
@@ -1,11 +1,18 @@
 package com.wonseok.stttextrate.biz.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.Gson;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -40,6 +47,9 @@ public class AnalysisReportVo {
     @Schema(title = "글자단위일치율", name = "charRate", description = "글자단위일치율", example = "100%")
     private String charRate;
     @JsonProperty
+    @Schema(title = "유사도일치율", name = "similarityRate", description = "자카드일치율", example = "100%")
+    private String similarityRate;
+    @JsonProperty
     @Schema(title = "문장단위일치율", name = "sentenceRate", description = "문장단위일치율", example = "100%")
     private String sentenceRate;
     @JsonProperty
@@ -48,15 +58,16 @@ public class AnalysisReportVo {
 
     @Builder
     public AnalysisReportVo(StringAnalysisVo val) {
-        this.answer = val.getAnswer();
+        this.answer = val.getAnswer().substring(0, 30) + "...";
         this.answerChar = val.getAnswerChar();
         this.answerSentence = val.getAnswerSentence();
-        this.sttAnswer = val.getSttAnswer();
+        this.sttAnswer = val.getSttAnswer().substring(0, 30) + "...";
         this.sttAnswerChar = val.getSttAnswerChar();
         this.sttAnswerSentence = val.getSttAnswerSentence();
         this.totalCharRate = totalCharRate(val);
         this.totalSentenceRate = totalSentenceRate(val);
         this.charRate = charRate(val);
+        this.similarityRate = similarityRate(val);
         this.sentenceRate = sentenceRate;
         this.textMeaningRate = textMeaningRate;
     }
@@ -83,38 +94,76 @@ public class AnalysisReportVo {
         int gap = (int) (answer.length() - sttAnswer.length());
         int originCount = answer.length();
         if (gap > 0) {
+            maxChar = new char[answer.toCharArray().length];
             maxChar = answer.toCharArray();
+            minChar = new char[sttAnswer.toCharArray().length];
             minChar = sttAnswer.toCharArray();
         } else {
+            maxChar = new char[sttAnswer.toCharArray().length];
             maxChar = sttAnswer.toCharArray();
+            minChar = new char[answer.toCharArray().length];
             minChar = answer.toCharArray();
             gap = gap * -1;
         }
         int flag = 0;
-        for (int i = 0; i < minChar.length - 1; i++) {
-            if (minChar[i] == maxChar[i + flag]) {
-                suc += 1;
-            } else {
-                int failflag =1;
-                for (int j = 0; j < gap; j++) {
-                    if (minChar[i] == maxChar[i + flag + j + 1]) {
-                        suc += 1;
-                        flag += 1;
-                        failflag = 0;
-                        break;
+        try {
+            for (int i = 0; i < minChar.length - 1; i++) {
+                if (minChar[i] == maxChar[i + flag]) {
+                    suc += 1;
+                } else {
+                    int failflag = 1;
+                    for (int j = 0; j < gap; j++) {
+                        int fullChar = 0;
+                        if (maxChar.length > i + flag + j + 1) {
+                            fullChar = maxChar.length;
+                        } else {
+                            fullChar = maxChar[fullChar];
+                        }
+                        if (minChar[i] == fullChar) {
+                            System.out.println(minChar[i] + "   " + maxChar[fullChar]);
+                            suc += 1;
+                            flag += 1;
+                            failflag = 0;
+                            break;
+                        }
                     }
+                    fail += failflag;
                 }
-                fail += failflag;
             }
+        } catch (Exception e) {
+            return "fail";
         }
-
-        System.out.println("gap = " + gap);
-        System.out.println("suc = " + suc);
-        System.out.println("fail = " + fail);
-        int max = suc + fail;
-        int min = suc;
-
-        return String.format("%.2f", Math.floor(originCount) / Math.floor(originCount+fail+gap) * 100.0) + "%";
+        return String.format("%.2f", Math.floor(originCount) / Math.floor(originCount + fail + gap) * 100.0) + "%";
     }
 
+    private String similarityRate(StringAnalysisVo dto) {
+        List<String> origin = new ArrayList<>(Arrays.stream(dto.getAnswer().split(" ")).distinct().toList());
+        List<String> stt = new ArrayList<>(Arrays.stream(dto.getSttAnswer().split(" ")).distinct().toList());
+        List<String> max = new ArrayList<>();
+        List<String> min = new ArrayList<>();
+        List<String> minDelList = new ArrayList<>();
+        int uCount = 0;
+        int nCount = 0;
+        try {
+            if (origin.size() > stt.size()) {
+                max = origin;
+                min = stt;
+            } else {
+                max = stt;
+                min = origin;
+            }
+            minDelList = new ArrayList<>(min);
+            for (String e : max) {
+                if (min.contains(e)) {
+                    nCount += 1;
+                    minDelList.remove(e);
+                } else {
+                    uCount += 1;
+                }
+            }
+        } catch (Exception e) {
+            return "fail";
+        }
+        return String.format("%.2f", (Math.floor(nCount) / Math.floor(nCount + minDelList.size() + uCount)) * 100) + "%";
+    }
 }
